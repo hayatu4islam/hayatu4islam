b81f6973f085805cf9286b7b666d0b8d702245f7
Hayat <hayatu4islam@yahoo.com>
2023-04-15 19:19:47 +0100

    add format

---
+++ Functions/format.py
@@ -0,0 +1,11 @@
+# Add a placholder
+# number = 25
+# number2 = 50
+# newmsg = "The number are {:.2f} and {} for you!"
+# print(newmsg.format(number, number2))
+
+# Using index numbers
+age = 25
+name = "Bolaji"
+newTxt = "The name is {1}. {1} is {0} years old."
+print(newTxt.format(age, name))
